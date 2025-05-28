094b2eac3c9150a309a5a50325c80da35eb6c9b1
siyuChen-gif <siyuche4@andrew.cmu.edu>
2025-05-27 21:38:32 -0400

    update on shell scripts

--- start_grafana.sh
+++ start_grafana.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# get port from gf_conn.yaml
-CONFIG_FILE="a_EverythingNeedToChange/gf_conn.yaml"
-GF_PORT=$(yq '.GF_PORT' "$CONFIG_FILE" | tr -d "'\"")
-GF_PORT=$((GF_PORT))
-GF_USER=$(yq '.GF_USER' "$CONFIG_FILE")
-GF_PASS=$(yq '.GF_PASS' "$CONFIG_FILE")
+# load info
+CONFIG_FILE="a_EverythingNeedToChange/gf_conn.yaml"     # define path
+GF_PORT=$(yq '.GF_PORT' "$CONFIG_FILE" | tr -d "'\"")   # port
+GF_PORT=$((GF_PORT))    # convert to int
+GF_USER=$(yq '.GF_USER' "$CONFIG_FILE")     # user name
+GF_PASS=$(yq '.GF_PASS' "$CONFIG_FILE")     # user password
 
 # navigate to grafana directory
 cd ~/grafana-v12.0.0/bin || { echo "Grafana not found"; exit 1; }
@@ -14,16 +14,16 @@ echo "Starting Grafana...(≧∇≦)"
 
 # Set enviornment
 export GF_SECURITY_ADMIN_USER="$GF_USER"
-export GF_SECURITY_ADMIN_PASSWORD="$GF_PASS"
+export GF_SECURITY_ADMIN_PASSWORD="$GF_PASS"    # auto login
 
-export GF_USERS_SIGN_UP=false
+export GF_USERS_SIGN_UP=false   # skip sign up
 
 export GF_AUTH_ANONYMOUS_ENABLED=true
-export GF_AUTH_ANONYMOUS_ORG_ROLE=Viewer
+export GF_AUTH_ANONYMOUS_ORG_ROLE=Viewer    # allow anonymous connetion
 
-export GF_AUTOFILL_PASSWORD=true
+export GF_AUTOFILL_PASSWORD=true    # auto sign up
 
-# Kill any previous Grafana instance on port 3000 
+# Kill any previous Grafana instance on port
 if lsof -i :$GF_PORT > /dev/null; then
     echo "Port $GF_PORT already in use (#ﾟдﾟ), killing previous process..."
     kill -9 $(lsof -ti :"$GF_PORT")
@@ -32,7 +32,7 @@ fi
 # Start Grafana server
 ./grafana server web > ../grafana_start.log 2>&1 &
 
-# Wait to give it time to start
+# Wait to give time to start
 sleep 2
 
 # Check if Grafana actually started (check port or process)
--- stop_grafana.sh
+++ stop_grafana.sh
@@ -4,15 +4,15 @@ echo "Stopping Grafana..."
 
 # get port from gf_conn.yaml
 CONFIG_FILE="a_EverythingNeedToChange/gf_conn.yaml"
-GF_PORT=$(yq '.GF_PORT' "$CONFIG_FILE" | tr -d "'\"")
-GF_PORT=$((GF_PORT))
+GF_PORT=$(yq '.GF_PORT' "$CONFIG_FILE" | tr -d "'\"")   # read from gf_conn
+GF_PORT=$((GF_PORT))    # convert to int
 
-# Listen to the operating port: 3000
+# Listen to the operating port
 PIDS=$(lsof -ti :$GF_PORT)
 
 if [ -z "$PIDS" ]; then
     echo "No Grafana process running on port $GF_PORT."
 else
     kill -9 $PIDS
-    echo "Grafana process(es) [$PIDS] stopped."
+    echo "Grafana processes stopped. (;´ヮ`)7"
 fi
