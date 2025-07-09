b511c30531b4bbe3532fdccf824a39b2fbba9587
JoyceXinyue <zhuangxinyue@outlook.com>
2025-07-09 16:46:58 -0400

    contributer reset

---
+++ create/create_alerts.py
@@ -0,0 +1,135 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import yaml
+
+from tool.helper import *
+from tool import AlertBuilder
+from tool import AlertRuleValidator
+
+"""
+This script generates all alert JSON files, saves them to a folder under `grafana_hgcdb_dashboard`, and uploads them to Grafana.
+Author: Xinyue (Joyce) Zhuang
+"""
+
+# Get the filelist from the config folder
+filelist = os.listdir(CONFIG_FOLDER_PATH)
+
+# Define the builder
+alert_builder = AlertBuilder(GF_DS_UID)
+
+# Delete all the alerts generated previously
+client.delete_all_alert_rules()
+
+# Define if success
+succeed = True      # assume every file success 
+failed_count = 0    # assume no file failed
+
+# Loop for every config files
+for config in filelist:
+
+    # skip non-yaml files
+    if not config.endswith(".yaml"):
+        continue
+
+    # config file path
+    config_path = os.path.join(CONFIG_FOLDER_PATH, config)
+    
+    # Load the alerts
+    with open(config_path, mode = 'r') as file:
+        tot_config = yaml.safe_load(file)
+        # check if alerts exist:
+        if "alert" not in tot_config:
+            continue
+        # load alerts
+        alerts = tot_config["alert"]
+    
+    try:
+        validator = AlertRuleValidator(config_path)
+        print(f"\n[VALIDATING] Checking if the alert rules in the config file: <{config}> is valid...")
+        valid_alerts = []
+    except FileNotFoundError as e:
+        print(f"[ERROR] {e}")
+        continue
+        
+    # Loop for every alert
+    for idx, alert in enumerate(alerts):
+        alert_title = alert.get("title", f"<Alert {idx}>")
+        ok = validator.check_single_alert(idx, alert)
+
+        if not ok:  # skip invalid config file
+            print(f"[WARNING] Validation failed for config: <{config}>. Skipping this file.\n")
+            succeed = False
+            failed_count += 1
+            continue
+        
+        # Generate the alert json
+        folder_name = config.split(".")[0].replace("_", " ")
+        alert_json = alert_builder.generate_alerts(alert, folder_name)
+
+        # Export the alert json to a file
+        file_name = config.split(".")[0]
+        alert_builder.save_alerts_json(alert, alert_json, file_name)
+
+if succeed:
+    print(" >>>> All Alerts json generated successfully!\n")
+else:
+    print(f" >>>> {failed_count} Alerts json failed to generate. \n")
+
+
+# Upload alerts
+try:
+    folder_list = os.listdir("./Alerts")
+    for folder in folder_list:
+        file_list = os.listdir(f"./Alerts/{folder}")
+        for file_name in file_list:
+            if file_name.endswith(".json"):
+                file_path = f"./Alerts/{folder}/{file_name}"
+                try:
+                    alert_builder.upload_alerts(file_path)
+                except Exception as e:
+                    print(f"[SKIPPED] Error uploading alert rule: {file_name} | Status: {e}")
+
+    print("\n >>>> Alerts json files uploaded!\n")
+
+except:
+    print("\n >>>> Alerts json files not found...\n")
+
+
+# clear all the notification rules and contact points
+client.delete_notification_policy_tree()
+client.delete_all_contact_points()
+
+# get file:
+try:
+    contact_filelist = os.listdir(CONTACT_FOLDER_PATH)
+except:
+    print(f"Contact config folder not found: {CONTACT_FOLDER_PATH}")
+
+for config in contact_filelist:
+    contact_path = os.path.join(CONTACT_FOLDER_PATH, config)
+
+    with open(contact_path, 'r') as f:
+            config = yaml.safe_load(f)
+
+    for cp in config.get("contactPoints", []):
+        client.create_contact_point(cp["name"], cp["addresses"])
+
+    # upload notification policy (contact points)
+    tree = config.get('policies')[0]
+    client.put_policy_tree(tree)
+
+    print("\n >>>> Notification system uploaded!\n")
+
+
+# Clear GF_FOLDER_UIDS and GF_ALERT_UIDS map:
+gf_conn.set("GF_FOLDER_UIDS", {})
+gf_conn.set("GF_ALERT_UIDS", {})
+gf_conn.save()
+gf_conn.reload()
+print(" >>>> GF_FOLDER_UIDS and GF_ALERT_UIDS map cleared!\n")
+
+# Delete the alert files
+remove_folder("Alerts", ALERTS_FOLDER_PATH)
+print("\n >>>> Alerts json files removed!\n")
---
+++ tool/builders/alert_builder.py
@@ -0,0 +1,181 @@
+from tool.helper import *
+
+"""
+This file defines the class for building the alert json file in Grafana and upload the alert.
+Author: Xinyue (Joyce) Zhuang
+"""
+
+class AlertBuilder:
+    def __init__(self, datasource_uid: str):
+        self.datasource_uid = datasource_uid
+
+    def generate_alerts(self, alert: dict, folder_name: str):
+        """Build all alerts based on the given alert_dict.
+        """ 
+        # generate alert json
+        alert_sql = self._generate_alertSQL(alert['parameter'], alert['table'])
+        alert_json = self.generate_alert_rule(alert_sql, alert, alert['dashboard'], folder_name)
+        
+        return alert_json
+
+    def generate_alert_rule(self, alertSQL: str, alertInfo: dict, dashboard_title: str, folder_name: str) -> dict:
+        """Generate a json of the alert rule based on the given info
+        """
+        folderName = folder_name.replace("_", " ")
+
+        # get dashboard uid
+        dashboard_uid = create_uid(dashboard_title)
+
+        # get alert rule uid
+        alert_uid = create_uid(f"ALERT {alertInfo['title']}")
+
+        # get folder uid
+        if folderName == "General":  # invalid folder
+            raise ValueError("[Error] Alert rule in invalid folder: General folder can not store alert rule.")
+        else:
+            folder_uid_map = gf_conn.get("GF_FOLDER_UIDS", {})
+            if folderName not in folder_uid_map:
+                raise ValueError(f"Dashboard Folder '{folderName}' not in GF_FOLDER_UIDS")
+            folder_uid = folder_uid_map[folderName]
+
+        # generate alert json
+        alert_json =  {
+            "title": f"ALERT: {alertInfo['title']}",
+            "uid": alert_uid,
+            "ruleGroup": alertInfo['title'],
+            "folderUID": folder_uid,
+            "condition": "C",
+            "orgId": 1,
+            "for": alertInfo["duration"],
+            "interval": alertInfo["interval"],
+            "noDataState": "NoData",
+            "execErrState": "Error",
+            "annotations": {
+                "__dashboardUid__": f"{dashboard_uid}",
+                "__panelId__": f"{alertInfo['panelID']}",
+                "summary": "Auto-imported from UI"
+            },
+            "labels":alertInfo["labels"],
+            "data": [
+                {
+                "refId": "A",
+                "relativeTimeRange": {
+                    "from": 600,
+                    "to": 0
+                },
+                "datasourceUid": self.datasource_uid,
+                "model": {
+                    "refId": "A",
+                    "format": "table",
+                    "rawQuery": "true",
+                    "rawSql": alertSQL
+                }
+                },
+                {
+                "refId": "B",
+                "relativeTimeRange": {
+                    "from": 0,
+                    "to": 0
+                },
+                "datasourceUid": "__expr__",
+                "model": {
+                    "expression": "A",
+                    "type": "reduce",
+                    "reducer": "last",
+                    "refId": "B"
+                }
+                },
+                {
+                "refId": "C",
+                "relativeTimeRange": {
+                    "from": 0,
+                    "to": 0
+                },
+                "datasourceUid": "__expr__",
+                "model": {
+                    "expression": "B",
+                    "type": "threshold",
+                    "refId": "C",
+                    "conditions": [
+                    {
+                        "type": "query",
+                        "evaluator": {
+                        "type": alertInfo["logicType"],
+                        "params": alertInfo["threshold"]
+                        },
+                        "operator": {
+                        "type": "and"
+                        },
+                        "query": {
+                        "params": ["C"]
+                        },
+                        "reducer": {
+                        "type": "last",
+                        "params": []
+                        }
+                    }
+                    ]
+                }
+            }
+            ]
+        } 
+
+        return alert_json
+    
+    def _generate_alertSQL(self, parameter: str, source: str) -> str:
+        alertSQL = f"""
+        SELECT
+            {parameter}
+        FROM
+            {source}
+        LIMIT 1;
+        """
+        return alertSQL
+    
+    def save_alerts_json(self, alert: dict, alert_json: dict, folder: str):
+        """Save alerts JSON into Alerts/<alerts_title>.json
+        """ 
+        # generate safe title for the filename
+        safe_title = alert["title"].replace(" ", "_")
+        filename = safe_title + ".json"
+
+        # create path
+        folder_path = os.path.join("Alerts", folder)
+        os.makedirs(folder_path, exist_ok=True)
+
+        path = os.path.join(folder_path, filename)
+
+        # import file
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(alert_json, f, indent=2)
+
+        print(f"Alerts saved to {path} \n")
+
+    def upload_alerts(self, file_path: str):
+        """Upload one alert JSON file into Grafana folder.
+        """
+        # reload gf_conn.yaml
+        gf_conn.reload()
+
+        # get folder and file name
+        folder_name = os.path.basename(os.path.dirname(file_path)).replace("_", " ")
+        file_name = os.path.basename(file_path).split(".")[0].replace("_", " ")
+        
+        # get folder uid
+        if folder_name == "General":  # main dashboards: empty uid
+            folder_uid = ""
+        else:
+            folder_uid_map = gf_conn.get("GF_FOLDER_UIDS", {})
+            if folder_name not in folder_uid_map:
+                raise ValueError(f"Dashboard Folder '{folder_name}' not in GF_FOLDER_UIDS")
+            folder_uid = folder_uid_map[folder_name]
+
+        with open(file_path, 'r', encoding='utf-8') as file:
+            alert_json = json.load(file)
+
+        # upload alerts
+        try:
+            client.upload_alert_json(alert_json, alert_json["uid"])
+
+        except requests.RequestException as e:
+            print(f"[ERROR] Failed to upload alert '{file_name}': {e}")
