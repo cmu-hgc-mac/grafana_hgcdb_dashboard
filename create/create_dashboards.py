62db67e42f49e152788d2b7769b52995c29fb07f
Sindhu Murthy <sindhumurthy97@gmail.com>
2026-08-03 15:02:36 +0200

    mmts sensors logging

--- create/create_dashboards.py
+++ create/create_dashboards.py
@@ -6,7 +6,7 @@ import yaml
 
 from tool.helper import *
 from tool import DashboardValidator
-from tool import PanelBuilder, FilterBuilder, InputBuilder, DashboardBuilder, ComponentsLookUpFormBuilder, HexmapPlotsBuilder, OffsetPlotsBuilder, GeneralInfoBuilder, ModuleAssemblyBuilder, XMLSuccessBuilder, ModuleGradesBuilder
+from tool import PanelBuilder, FilterBuilder, InputBuilder, DashboardBuilder, ComponentsLookUpFormBuilder, HexmapPlotsBuilder, OffsetPlotsBuilder, GeneralInfoBuilder, ModuleAssemblyBuilder, XMLSuccessBuilder, ModuleGradesBuilder, MMTSLoggingBuilder
 
 """
 This script generates all the dashboards json_file, saves them to a folder under `grafana_hgcdb_dashboard`, and uploads them to grafana.
@@ -27,6 +27,7 @@ general_info_builder = GeneralInfoBuilder(GF_DS_UID, TIME_ZONE)
 module_assembly_builder = ModuleAssemblyBuilder(GF_DS_UID, TIME_ZONE)
 xml_success_builder = XMLSuccessBuilder(GF_DS_UID, TIME_ZONE)
 module_grades_builder = ModuleGradesBuilder(GF_DS_UID, TIME_ZONE)
+mmts_logging_builder = MMTSLoggingBuilder(GF_DS_UID, TIME_ZONE)
 
 # Check if succeed:
 succeed = True      # assert every file generated successfully
@@ -116,6 +117,13 @@ for config in filelist:
 
         elif dashboard_title == "Module Grades":
             dashboard_json = module_grades_builder.generate_dashboard_json()
+            # Export the dashboard json to a file
+            file_name = config.split(".")[0]
+            dashboard_builder.save_dashboard_json(dashboard, dashboard_json, file_name)
+            continue
+
+        elif dashboard_title == "MMTS Environment Logging":
+            dashboard_json = mmts_logging_builder.generate_dashboard_json()
             # Export the dashboard json to a file
             file_name = config.split(".")[0]
             dashboard_builder.save_dashboard_json(dashboard, dashboard_json, file_name)
--- tool/builders/__init__.py
+++ tool/builders/__init__.py
@@ -2,3 +2,4 @@ from .alert_builder import *
 from .dashboard_builder import *
 from .other_builder import *
 from .panel_builder import *
+from .mmts_logging_builder import *
