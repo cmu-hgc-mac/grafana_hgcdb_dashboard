80f1fe989ec0165b10cba66a3e5556fd8020a549
Sindhu Murthy <sindhumurthy97@gmail.com>
2026-09-22 19:19:03 -0400

    green >0

--- tool/builders/module_assembly_builder.py
+++ tool/builders/module_assembly_builder.py
@@ -693,12 +693,25 @@ class ModuleAssemblyBuilder:
                             },
                             {
                                 "options": {
-                                "0": {
+                                "from": None,
+                                "result": {
                                     "color": "red",
                                     "index": 1
-                                }
                                 },
-                                "type": "value"
+                                "to": 0
+                                },
+                                "type": "range"
+                            },
+                            {
+                                "options": {
+                                "from": 1,
+                                "result": {
+                                    "color": "green",
+                                    "index": 2
+                                },
+                                "to": None
+                                },
+                                "type": "range"
                             }
                             ]
                         }
